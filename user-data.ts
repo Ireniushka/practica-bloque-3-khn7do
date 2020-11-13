c40c2bf84a4c845cac59517513f16d53bca74c5c
Ireniushka <irenerengelruiz@gmail.com>
2020-11-13 21:46:34 +0100

    primer commit,  interfaz y primer método realizado

---
+++ user-data.ts
@@ -0,0 +1,11 @@
+// En esta práctica debéis crear una interfaz con el nombre "UserData" en el archivo "user-data.ts". En esta interfaz, debéis añadir las propiedades "name", "age" y "phone" con los tipos cadena de texto, número y número respectivamente. Tenéis que exportar esta interfaz.
+
+interface UserData {
+  //Atributos
+  name:string;
+  age: number;
+  phone: number;
+
+}
+
+export {UserData};
